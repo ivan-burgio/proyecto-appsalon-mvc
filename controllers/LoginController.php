01985226e40b1f0ffa80a6ddd616c9a9dd93411d
ivan-burgio <ivan.24.burgio@gmail.com>
2023-04-26 21:42:08 -0300

    Añadir boton para cerrar sesion

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -51,7 +51,9 @@ class LoginController {
     }
 
     public static function logout() {
-        echo "desde logout";
+        session_start();
+        $_SESSION = [];
+        header('Location: /');
     }
 
     public static function olvide(Router $router) {
