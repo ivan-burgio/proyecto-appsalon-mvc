a94380f1e3851f6986eeae5b510dbbdd191731e8
ivan-burgio <ivan.24.burgio@gmail.com>
2023-05-04 22:11:02 -0300

    Creando el formulario para servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -1,3 +1,8 @@
 <h1 class="nombre-pagina">Nuevo servicio</h1>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+    <input type="submit" class="boton" value="Guardar servicios">
+</form>
