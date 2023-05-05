9333de8b9f5e73b7f788374e4fc2e7ecfaef5ca4
ivan-burgio <ivan.24.burgio@gmail.com>
2023-05-04 21:42:50 -0300

    Routing para servicios

---
+++ controllers/ServicioController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class ServicioController {
+    public static function index(Router $router) {
+        $router->render('servicios/index', [
+
+        ]);
+    }
+
+    public static function crear(Router $router) {
+
+        if($_SESSION['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    public static function actualizar(Router $router) {
+
+        if($_SESSION['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function eliminar(Router $router) {
+        if($_SESSION['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+}
--- views/templates/barra.php
+++ views/templates/barra.php
@@ -4,10 +4,10 @@
     <a class="boton" href="/logout">Cerrar sesión</a>
 </div>
 
-<php if(isset($_SESSION['admin'])) { ?>
+<?php if(isset($_SESSION['admin'])) { ?>
     <div class="barra-servicios">
         <a href="/admin" class="boton">Ver citas</a>
         <a href="/servicios" class="boton">Ver servicios</a>
         <a href="/servicios/crear" class="boton">Nuevo servicio</a>
     </div>
-<php } ?>
+<?php } ?>
