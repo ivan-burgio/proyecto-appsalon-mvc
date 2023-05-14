03c6958693f79e9d2570bef8e00a47fce6e820b6
ivan-burgio <ivan.24.burgio@gmail.com>
2023-05-14 17:42:48 -0300

    arreglado enserio

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,11 @@
 <?php
 
-function conectarDB() : mysqli {
-    $db = new mysqli($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASS'] ?? '',$_ENV['DB_BD']);
+$db = mysqli_connect('localhost', 'root', '!Mysql248910', 'appsalon_mvc');
 
-    if (!$db) {
-        echo "Error, no se pudo conectar";
-        exit;
-    }
-    return $db;
+
+if(!$db) {
+    echo "Error: No se pudo conectar a MySQL.";
+    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
+    exit;
 }
