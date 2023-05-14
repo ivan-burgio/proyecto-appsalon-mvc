f59b98f1fd351ff1e95049d1da823d41ef7efe67
ivan-burgio <ivan.24.burgio@gmail.com>
2023-05-14 17:35:33 -0300

    env arreglado

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,11 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASS'] ?? '',$_ENV['DB_BD']);
+function conectarDB() : mysqli {
+    $db = new mysqli($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASS'] ?? '',$_ENV['DB_BD']);
 
-
-if(!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "error de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
-    exit;
+    if (!$db) {
+        echo "Error, no se pudo conectar";
+        exit;
+    }
+    return $db;
 }
